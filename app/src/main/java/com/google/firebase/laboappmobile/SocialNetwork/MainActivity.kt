185ba269a37c0da0b8eb6565b704365fc99bb530
amna <a6harkat@enib.fr>
2022-06-16 22:21:17 +0200

    getUser()

--- app/src/main/java/com/google/firebase/laboappmobile/SocialNetwork/MainActivity.kt
+++ app/src/main/java/com/google/firebase/laboappmobile/SocialNetwork/MainActivity.kt
@@ -1,17 +1,31 @@
 package com.google.firebase.laboappmobile.SocialNetwork
 
+import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import androidx.appcompat.app.AppCompatActivity
-import android.R.layout
 import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI.IdpConfig.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.laboappmobile.SocialNetwork.databinding.ActivityMainBinding
+
+import com.google.firebase.database.ktx.database
 
 class MainActivity : AppCompatActivity(){
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var db: FirebaseDatabase
     private lateinit var adapter: ArrayAdapter<*>
     override fun onCreate(savedInstanceState: Bundle?) {
+        db = Firebase.database
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         setContentView(R.layout.activity_main)
         val lv_listView = findViewById(R.id.lv_listView) as ListView
         val tv_emptyTextView = findViewById(R.id.tv_emptyTextView) as TextView
@@ -21,7 +35,34 @@ class MainActivity : AppCompatActivity(){
             Toast.makeText(applicationContext,parent?.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show()
         }
         lv_listView.emptyView = tv_emptyTextView
+
+        auth = Firebase.auth
+        if (auth.currentUser == null) {
+            // Not signed in, launch the Sign In activity
+            startActivity(Intent(this, SignInActivity::class.java))
+            finish()
+            return
+        }
+        getUser()
+
     }
+
+    fun getUser(){
+        var messageEditText = findViewById(R.id.messageEditText) as TextView
+        val user = auth.currentUser
+        if (user != null) {
+            db.reference.child("socialUsers").get().addOnSuccessListener {
+                Log.i("firebase", "Got value ${it.value}")
+                messageEditText.setText(it.value.toString())
+            }.addOnFailureListener {
+                Log.e("firebase", "Error getting data", it)
+
+            }
+        }
+
+
+    }
+
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main_menu,menu)
@@ -41,4 +82,12 @@ class MainActivity : AppCompatActivity(){
         })
         return super.onCreateOptionsMenu(menu)
     }
+
+
+
+
+
+
+
+
 }
