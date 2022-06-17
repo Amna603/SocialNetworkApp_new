ecc73f9e5c1381bf00ffea1bee3bfbb8e165a73c
amna <a6harkat@enib.fr>
2022-06-17 12:04:32 +0200

    Rien à avoir avec avant

--- app/src/main/java/com/google/firebase/laboappmobile/SocialNetwork/MainActivity.kt
+++ app/src/main/java/com/google/firebase/laboappmobile/SocialNetwork/MainActivity.kt
@@ -1,98 +1,99 @@
 package com.google.firebase.laboappmobile.SocialNetwork
 
-import android.content.Intent
-import android.os.Bundle
+import android.content.ContentValues.TAG
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.*
-import androidx.appcompat.app.AppCompatActivity
-import com.firebase.ui.auth.AuthUI.IdpConfig.*
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.GenericTypeIndicator
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.laboappmobile.SocialNetwork.databinding.ActivityMainBinding
+import com.google.firebase.laboappmobile.SocialNetwork.model.User
+import org.w3c.dom.Text
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.database
+
+import androidx.fragment.app.Fragment
+
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.fragment_profile.view.*
 
 
-class MainActivity : AppCompatActivity(){
-    private lateinit var auth: FirebaseAuth
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var db: FirebaseDatabase
-    private lateinit var adapter: ArrayAdapter<*>
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.laboappmobile.SocialNetwork.Fragments.*
+
+import androidx.fragment.app.FragmentManager
+import com.google.firebase.laboappmobile.SocialNetwork.AddPostActivity
+import com.google.firebase.laboappmobile.SocialNetwork.Fragments.HomeFragment
+import com.google.firebase.laboappmobile.SocialNetwork.Fragments.ProfileFragment
+import com.google.firebase.laboappmobile.SocialNetwork.Fragments.SearchFragment
+
+class MainActivity : AppCompatActivity() {
+
+    internal var selectedFragment:Fragment?=null
+
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.nav_home -> {
+                moveToFragment(HomeFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_search -> {
+                moveToFragment(SearchFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_addpost -> {
+                item.isChecked=false
+                startActivity(Intent(this@MainActivity, AddPostActivity::class.java))
+                return@OnNavigationItemSelectedListener true
+            }
+            /*R.id.nav_notifications -> {
+                moveToFragment(NotificationFragment())
+                return@OnNavigationItemSelectedListener true
+            }*/
+            R.id.nav_profile -> {
+                moveToFragment(ProfileFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        db = Firebase.database
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
         setContentView(R.layout.activity_main)
-        val lv_listView = findViewById(R.id.lv_listView) as ListView
-        val tv_emptyTextView = findViewById(R.id.tv_emptyTextView) as TextView
-        adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1, resources.getStringArray(R.array.users_array_test))
-        lv_listView.adapter = adapter
-        lv_listView.onItemClickListener = AdapterView.OnItemClickListener{parent, view, position, id ->
-            Toast.makeText(applicationContext,parent?.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show()
-        }
-        lv_listView.emptyView = tv_emptyTextView
+        setSupportActionBar(findViewById(R.id.home_toolbar))
 
-        auth = Firebase.auth
-        if (auth.currentUser == null) {
-            // Not signed in, launch the Sign In activity
-            startActivity(Intent(this, SignInActivity::class.java))
-            finish()
-            return
-        }
-        getUser()
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
 
+        val publisher = intent.getStringExtra("PUBLISHER_ID")
+        if(publisher!=null) {
+            val prefs: SharedPreferences.Editor? =
+                getSharedPreferences("PREFS", Context.MODE_PRIVATE)
+                    .edit().apply { putString("profileId", publisher); apply() }
+
+            moveToFragment(ProfileFragment())
+        }
+        else
+        //to call fragments
+            moveToFragment(HomeFragment())
     }
 
-    fun getUser(){
-        var messageEditText = findViewById(R.id.messageEditText) as TextView
-        val user = auth.currentUser
-        if (user != null) {
-
-           db.reference.child("socialUsers").get().addOnSuccessListener {
-
-               Log.i("firebase", "Got value ${it.value}")
-                    messageEditText.setText(it.value.toString())
-
-                //val listUsers: MutableList<SocialUser> = it.value<SocialUser>
-            }.addOnFailureListener {
-                Log.e("firebase", "Error getting data", it)
-
-            }
-        }
-
-
+    private fun moveToFragment(fragment:Fragment)
+    {
+        val fragmentTrans=supportFragmentManager.beginTransaction()
+        fragmentTrans.replace(R.id.fragment_container,fragment)
+        fragmentTrans.commit()
     }
-
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.main_menu,menu)
-
-        val search :MenuItem? = menu?.findItem(R.id.nav_search)
-        val searchView :SearchView = search?.actionView as SearchView
-        searchView.queryHint = "Search something!"
-
-        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                return true
-            }
-
-            override fun onQueryTextChange(newText: String?): Boolean {
-                return false
-            }
-        })
-        return super.onCreateOptionsMenu(menu)
-    }
-
-
-
-
-
-
-
-
 }
